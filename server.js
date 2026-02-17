e2a8424e74e56a45ca98f99308af10bde9e12ce3
Timeo Dave <seyapdjetimeo@gmail.com>
2026-02-18 00:11:21 +0100

    Fix - réajout import nodemailer

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const { Pool } = require('pg');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
